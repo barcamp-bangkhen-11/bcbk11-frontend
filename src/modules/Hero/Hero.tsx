4d20b378c614b85d9dfba22828e18c1d8ceccdcd
karitthorn <56643494+karitthorn@users.noreply.github.com>
2023-10-12 16:05:38 +0700

    fix hover,heading,chageImgLocation

--- src/modules/Hero/Hero.tsx
+++ src/modules/Hero/Hero.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Section from "../components/Section";
 import Flare from "../components/Flare";
 import Image from "next/image";
-import barcampHero from "@/modules/img/barcampHero.png";
+import barcampHero from "../../../public/images/barcampHero.png";
 import { useState } from "react";
 import { useEffect } from "react";
 import Link from "next/link";
@@ -11,18 +11,19 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 const Hero = () => {
   const [countdown, setCountdown] = useState<{
-    days: number;
-    hours: number;
-    minutes: number;
-    seconds: number;
+    days: string;
+    hours: string;
+    minutes: string;
+    seconds: string;
   }>({
-    days: 0,
-    hours: 0,
-    minutes: 0,
-    seconds: 0,
+    days: "0",
+    hours: "0",
+    minutes: "0",
+    seconds: "0",
   });
 
   useEffect(() => {
+    const pad = (n: number) => (n < 10 ? `0${n}` : n.toString());
     const targetDate = new Date("2023-11-25T00:00:00Z"); // 25/11/2023
 
     const intervalId = setInterval(() => {
@@ -36,11 +37,16 @@ const Hero = () => {
       const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
       const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
-      setCountdown({ days, hours, minutes, seconds });
+      setCountdown({
+        days: pad(days),
+        hours: pad(hours),
+        minutes: pad(minutes),
+        seconds: pad(seconds),
+      });
 
       if (difference < 0) {
         clearInterval(intervalId);
-        setCountdown({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+        setCountdown({ days: "00", hours: "00", minutes: "00", seconds: "00" });
       }
     }, 1000);
 
@@ -64,12 +70,12 @@ const Hero = () => {
               alt="barcamp"
             />
 
-            <div className="mt-15 flex flex-col items-center justify-center pt-5 md:ml-24 md:pt-0">
-              <div className="mb-12 text-center text-[50px]  font-normal text-white lg:text-[84px]">
+            <div className="mt-15 flex flex-col items-center justify-center pt-5 md:pt-0 lg:ml-24">
+              <div className="mb-12 text-center text-desktop-h3  font-normal text-white  md:text-desktop-h1">
                 {countdown.days}:{countdown.hours}:{countdown.minutes} day
               </div>
               <Link href="">
-                <div className="inline-flex h-[60px] items-center justify-center rounded-[45px] border-4 border-stone-300 from-orange-400 via-red-500 to-transparent px-[40px]  py-5 shadow-xl hover:bg-gradient-to-r hover:drop-shadow-2xl md:h-[76px] md:w-[316px]">
+                <div className="inline-flex h-[60px] items-center justify-center rounded-[45px] border-4 border-stone-300 px-[40px] py-5 shadow-xl  hover:border-white hover:bg-purple-500  hover:drop-shadow-2xl md:h-[76px] md:w-[316px]">
                   <div className="font-bold text-white ">
                     <h1 className=" w-full  text-[25px] md:text-[33px] ">
                       Register Now
