03e6f67154607759ef74102dd5be3f3dd9507dc6
GolfGrab <surapus01@gmail.com>
2023-10-13 21:42:09 +0700

    add logo image navbar blur and favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,11 +11,7 @@ import Head from "next/head";
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Barcamp Bangkhen 11</title>
-        <meta name="description" content="Barcamp Bangkhen 11" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <CustomHead />
       <main className="min-h-screen overflow-hidden bg-[#262329] text-white">
         <Navbar />
         <Hero />
@@ -29,3 +25,37 @@ export default function Home() {
     </>
   );
 }
+
+const CustomHead = () => (
+  <Head>
+    <title>Barcamp Bangkhen 11</title>
+    <meta name="description" content="Barcamp Bangkhen 11" />
+    <meta name="theme-color" content="#262329" />
+    <link rel="icon" href="/favicon/favicon.ico" />
+    <link
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="/favicon/apple-touch-icon.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="/favicon/favicon-32x32.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href="/favicon/favicon-16x16.png"
+    />
+    <link rel="manifest" href="/favicon/site.webmanifest" />
+    <link
+      rel="mask-icon"
+      href="/favicon/safari-pinned-tab.svg"
+      color="#262329"
+    />
+    <meta name="msapplication-TileColor" content="#9f00a7" />
+    <meta name="theme-color" content="#262329" />
+  </Head>
+);
